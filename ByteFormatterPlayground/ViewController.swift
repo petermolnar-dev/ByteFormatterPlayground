58bccd2ef95f31ec37c882ba4700d74183019de2
Peter Molnar <petermolnar.hu@gmail.com>
2020-10-01 19:40:48 +0200

    Initial Commit

---
+++ ByteFormatterPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteFormatterPlayground
+//
+//  Created by Peter Molnar on 01/10/2020.
+//  Copyright © 2020 Peter Molnar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
