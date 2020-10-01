2db46f885f5eb721742dd36f380a5b5978ec9642
Peter Molnar <petermolnar.hu@gmail.com>
2020-10-01 20:01:17 +0200

    Moving the extension in place.

--- Int+normaliseFromByte.swift
+++ Int+normaliseFromByte.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 
+extension Int {
+    
+    /// Giving back a normalised  value and the measuremen unit from the Int amount, assuming that the amount is represented is byte.
+    /// Normalisation is done with 2 decimals.
+    /// Examlpes:
+    ///
+    /// Self value = 1163919, normaliseFromByte() -> "1.11 MB"
+    /// Self value = 1, normaliseFromByte() -> "1.00 byte"
+    /// Self.value = 1024 , normaliseFromByte() -> "1.00 KB"
+    /// Selv.value = 1610612736, normaliseFromByte() -> "1.50 GB"
+    public func normaliseFromByte() -> String {
+        let formatter = ByteCountFormatter()
+        formatter.countStyle = .binary
+        formatter.allowsNonnumericFormatting = false
+        formatter.isAdaptive = false
+        formatter.zeroPadsFractionDigits = true
+        
+        return formatter.string(fromByteCount: Int64(self))
+    }
+}
